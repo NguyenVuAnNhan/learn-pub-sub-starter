5cf12705de586b0fd276ceef71e197a0cf2cf580
NguyenVuAnNhan <annhan0328@gmail.com>
2026-02-25 21:44:03 +1100

    feat: L1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const connectionString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	fmt.Println("Connected to RabbitMQ")
+	
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(sigCh)
+
+	<- sigCh
+
+	fmt.Println("Shutting down Peril server...")
 }
