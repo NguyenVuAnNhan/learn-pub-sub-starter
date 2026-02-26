9889225e7e80bf508c6a35cf4468a34509e6d42d
NguyenVuAnNhan <annhan0328@gmail.com>
2026-02-26 16:06:15 +1100

    feat: L4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,40 +8,63 @@ import (
 	"syscall"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 )
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 
-	const connectionString = "amqp://guest:guest@127.0.0.1:5672/"
-	conn, err := amqp.Dial(connectionString)
+	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
-	fmt.Println("Connected to RabbitMQ")
+	fmt.Println("Peril game server connected to RabbitMQ!")
 
-	channel, err := conn.Channel()
+	publishCh, err := conn.Channel()
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not create channel: %v", err)
 	}
-	defer channel.Close()
-	fmt.Println("Channel opened")
-	
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	defer signal.Stop(sigCh)
 
-	state := routing.PlayingState{
-		IsPaused: true,
+	gamelogic.PrintServerHelp()
+
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "pause":
+			fmt.Println("Publishing paused game state")
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+		case "resume":
+			fmt.Println("Publishing resumes game state")
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+		case "quit":
+			log.Println("goodbye")
+			return
+		default:
+			fmt.Println("unknown command")
+		}
 	}
-	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, state)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Published initial paused state")
-
-	<- sigCh
-
-	fmt.Println("Shutting down Peril server...")
 }
